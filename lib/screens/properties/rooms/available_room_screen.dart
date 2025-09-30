76db18c7e1ba89b19215b8159721402b2b990610
joe2sure <joe2sure1@gmail.com>
2025-09-30 13:06:55 +0100

    fix: available_room_screen

---
+++ lib/screens/properties/rooms/available_room_screen.dart
@@ -0,0 +1,41 @@
+import 'package:Peeman/providers/room_provider.dart';
+import 'package:Peeman/screens/properties/rooms/room_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+class AvailableRoomsScreen extends StatefulWidget {
+  const AvailableRoomsScreen({super.key});
+
+  @override
+  State<AvailableRoomsScreen> createState() => _AvailableRoomsScreenState();
+}
+
+class _AvailableRoomsScreenState extends State<AvailableRoomsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<RoomProvider>(context, listen: false).fetchAvailableRooms(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final roomProvider = Provider.of<RoomProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Available Rooms'),
+      ),
+      body: roomProvider.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : roomProvider.rooms.isEmpty
+              ? const Center(child: Text('No available rooms found'))
+              : Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: RoomCard(rooms: roomProvider.rooms),
+                ),
+    );
+  }
+}
